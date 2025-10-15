8fc09ff7a334787a9dc83a4df27d31a24ae9d72f
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2025-10-15 09:06:48 +0800

    qwewqe

--- server/src/main.ts
+++ server/src/main.ts
@@ -72,6 +72,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(
